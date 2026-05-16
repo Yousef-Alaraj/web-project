3660887b1d46c2c9f2ac350828f76374e6173eed
Yousef Alaraj <yousefelaraj@gmail.com>
2026-05-16 20:04:00 +0300

    fixed typos and stuff

--- discover.php
+++ discover.php
@@ -8,7 +8,7 @@ require_once 'db_config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Discover</title>
 </head>
 <body>
     <header>
--- places.php
+++ places.php
@@ -7,7 +7,7 @@ require_once 'db_config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>places</title>
+    <title>Places</title>
     <link rel="stylesheet" href="places.css">
 
     
