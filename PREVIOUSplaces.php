7d25a2130f63484c76f8bcba2a9e33dd09e24312
Yousef Abu Khass <yousefabukhass@hotmail.com>
2026-05-18 05:59:52 +0300

    f

--- PREVIOUSplaces.php
+++ PREVIOUSplaces.php
@@ -1,4 +1,5 @@
 
+
 <?php
 session_start();
 require_once 'db_config.php';
@@ -8,7 +9,7 @@ require_once 'db_config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>places</title>
+    <title>Places</title>
     <link rel="stylesheet" href="places.css">
 
     
@@ -34,10 +35,10 @@ require_once 'db_config.php';
 
             <nav>
                 <ul id="navigation">
-                    <li><a href="index.html" class="nav-link">Home</a></li>
-                    <li><a href="places.html" class="nav-link">Places</a></li>
-                    <li><a href="discover.html" class="nav-link">Discover</a></li>
-                    <li><a href="contact.html" class="nav-link">Contact Us</a></li>
+                    <li><a href="index.php" class="nav-link">Home</a></li>
+                    <li><a href="places.php" class="nav-link">Places</a></li>
+                    <li><a href="discover.php" class="nav-link">Discover</a></li>
+                    <li><a href="contact.php" class="nav-link">Contact Us</a></li>
                     <?php if (isset($_SESSION['user_id'])): ?>
                         <li><a href="dashboard.php" class="nav-link">Dashboard</a></li>
                         <li><a href="logout.php" class="nav-link">Logout</a></li>
@@ -46,7 +47,7 @@ require_once 'db_config.php';
                     <?php endif; ?>
                 </ul>
             </nav>
-             <?php if (isset($_SESSION['username'])): ?>
+            <?php if (isset($_SESSION['username'])): ?>
                 <div class="user-greeting">
                     <span class="user-icon"><?php echo strtoupper(substr($_SESSION['username'], 0, 1)); ?></span>
                     <span class="user-name"><?php echo htmlspecialchars($_SESSION['username']); ?></span>
--- places.php
+++ places.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 require_once 'db_config.php';
@@ -7,7 +8,7 @@ require_once 'db_config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Places</title>
+    <title>places</title>
     <link rel="stylesheet" href="places.css">
 
     
@@ -33,10 +34,10 @@ require_once 'db_config.php';
 
             <nav>
                 <ul id="navigation">
-                    <li><a href="index.php" class="nav-link">Home</a></li>
-                    <li><a href="places.php" class="nav-link">Places</a></li>
-                    <li><a href="discover.php" class="nav-link">Discover</a></li>
-                    <li><a href="contact.php" class="nav-link">Contact Us</a></li>
+                    <li><a href="index.html" class="nav-link">Home</a></li>
+                    <li><a href="places.html" class="nav-link">Places</a></li>
+                    <li><a href="discover.html" class="nav-link">Discover</a></li>
+                    <li><a href="contact.html" class="nav-link">Contact Us</a></li>
                     <?php if (isset($_SESSION['user_id'])): ?>
                         <li><a href="dashboard.php" class="nav-link">Dashboard</a></li>
                         <li><a href="logout.php" class="nav-link">Logout</a></li>
@@ -45,7 +46,7 @@ require_once 'db_config.php';
                     <?php endif; ?>
                 </ul>
             </nav>
-            <?php if (isset($_SESSION['username'])): ?>
+             <?php if (isset($_SESSION['username'])): ?>
                 <div class="user-greeting">
                     <span class="user-icon"><?php echo strtoupper(substr($_SESSION['username'], 0, 1)); ?></span>
                     <span class="user-name"><?php echo htmlspecialchars($_SESSION['username']); ?></span>
